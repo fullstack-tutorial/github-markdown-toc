4b50f4bceb1ad80b727ffdf990c6fafa9bc27bb3
Frank <1002039945@qq.com>
2018-12-04 12:59:09 +0800

    修改  toc 样式

--- js/content-script.js
+++ js/content-script.js
@@ -1,60 +1,69 @@
-const $ = function(str) {
-  return document.querySelector(str);
+const $ = function (str) {
+    return document.querySelector(str);
 };
 
-setTimeout(function(){
-	var git_reg=/[https://github.com].*/;
-	if(git_reg.test(document.URL)){
-		var _readme=$('article');
-		var data=_readme.querySelectorAll('h2,h3,h4,h5,h6');;
-		var div=init(data);
-		document.body.appendChild(div);
-	}
-},1000)
+setTimeout(function () {
+    var git_reg = /[https://github.com].*/;
+    if (git_reg.test(document.URL)) {
+        var _readme = $('article');
+        var data = _readme.querySelectorAll('h1,h2,h3,h4,h5,h6');;
+        var div = init(data);
+        document.body.appendChild(div);
+    }
+}, 1000)
 
-function init(list){
-	var toc = document.createElement("ul");
-        addStyle(toc,{
-			"position": "fixed",
-			"width": "150px",
-			"min-height":"200px",
-			"z-index": 999,
-			"right": "0px",
-			"top": "60px",
-			"background": "rgba(33,55,61,0.7)"
-        })
-	var stack = new Array();
- for (var i = 0; i < list.length; i++) {
+function init(list) {
+    var toc = document.createElement("ul");
+    addStyle(toc, {
+        "position": "fixed",
+        "margin-right": "20px",
+        "padding": "20px",
+        "border-radius": "3px",
+        "width": "400px",
+        "height": "85%",
+        "min-height": "200px",
+        "z-index": 999,
+        "left": "10px",
+        "top": "70px",
+        "bottom": "20px",
+        "overflow-x": "hidden",
+        "overflow": "auto",
+        // "background": "gainsboro",
+        "border": "1px solid #d1d5da",
+    })
+    var stack = new Array();
+    for (var i = 0; i < list.length; i++) {
         var header = list[i];
-        var level = parseInt(header.tagName.replace('H', ''), 10)-1;
+        var level = parseInt(header.tagName.replace('H', ''), 10) - 1;
         // 通过两个where循环对栈进行调整,确保stack中标题级数与当前标题级数相同
-        while(stack.length < level){
+        while (stack.length < level) {
             stack.push(0);
         }
-        while(stack.length > level){
+        while (stack.length > level) {
             stack.pop();
         }
-        // 最小一级标题标号步进+1
-        stack[stack.length-1]++;
+
+        // 最小一级标题标号步进 + 1
+        stack[stack.length - 1]++;
         // 生成标题标号( 1.1,1.2.. )
         var index = stack.join(".")
         // 生成标题ID
         var id = "title" + index;
-        header.setAttribute('id',id);
-        var li=document.createElement("li");
-        addStyle(li,{
-        	"listStyle":"none",
+        header.setAttribute('id', id);
+        var li = document.createElement("li");
+        addStyle(li, {
+            "listStyle": "none",
         })
         toc.appendChild(li);
         var a = document.createElement("a");
-        addStyle(a,{
-        	"color":"rgb(255,254,248)",
-        	"textOverflow ":"ellipsis"
+        addStyle(a, {
+            "color": "#0366d6",
+            "textOverflow ": "ellipsis"
         })
         // 为目录项设置链接
         a.setAttribute("href", "#" + id)
         // 目录项文本前面放置缩进空格
-        a.innerHTML =new Array(level * 4).join('&nbsp;')+ index+new Array( 2).join('&nbsp;')+header.textContent;
+        a.innerHTML = new Array(level * 4).join('&nbsp;') + index + new Array(2).join('&nbsp;') + header.textContent;
         toc.lastChild.appendChild(a);
     }
     return toc;
@@ -62,8 +71,8 @@ function init(list){
 }
 
 
-function addStyle(obj,atrr){
-	for(var pro in atrr){
-		obj.style[pro]=atrr[pro];
-	}
+function addStyle(obj, atrr) {
+    for (var pro in atrr) {
+        obj.style[pro] = atrr[pro];
+    }
 }
