2da6f0bc9a1507173393cfb883224dd02f937d33
Frank <1002039945@qq.com>
2021-07-24 20:31:13 +0800

    修复插件bug

--- markdown-toc-code/js/background.js
+++ markdown-toc-code/js/background.js
@@ -1,11 +1,51 @@
- chrome.browserAction.setBadgeText({text: 'new'});
- chrome.browserAction.setBadgeBackgroundColor({color: [255, 0, 0, 255]});
+chrome.browserAction.setBadgeText({ text: 'new' });
+chrome.browserAction.setBadgeBackgroundColor({ color: [255, 0, 0, 255] });
 chrome.browserAction.onClicked.addEventListener(claaback);
-function claaback(){
+
+function claaback() {
 	console.log(1);
 }
 chrome.runtime.onMessage.addEventListener(bk);
-function bk(){
+function bk() {
 	console.log("hello")
 }
+
+
+
+// // 右键菜单管理
+// chrome.contextMenus.create({
+// 	"type": "radio",
+// 	"title": "切换绝对路径抓取",
+// 	"checked": false,
+// 	"onclick": switch_absolute_xpath   // 点击时调用方法
+// });
+// chrome.contextMenus.create({
+// 	"type": "radio",
+// 	"title": "切换NCC路径抓取",
+// 	"checked": true,
+// 	"onclick": switch_ncc_xpath
+// });
+// // 参数会传递 tab 信息，通过 tab.id 可以获取用户是在到那个页面里点击我们菜单选项的
+// function switch_absolute_xpath(info, tab) {
+// 	// 然后我通过 tab.id 给对应页面传递消息
+// 	chrome.tabs.sendMessage(tab.id, { type: 'switch_absolute_xpath' });
+// }
+// function switch_ncc_xpath(info, tab) {
+// 	chrome.tabs.sendMessage(tab.id, { type: 'switch_ncc_xpath' });
+// }
+
+
+chrome.contextMenus.create({
+	id: 'baidu-search',
+	title: '使用度娘搜索：%s',
+	contexts: ['selection']
+});
+chrome.contextMenus.onClicked.addListener(function (info, tab) {
+	switch (info.menuItemId) {
+		case 'baidu-search':
+			chrome.tabs.create({ url: 'https://www.baidu.com/s?ie=utf-8&wd=' + encodeURI(info.selectionText) });
+			break;
+	}
+});
+
 
--- markdown-toc-code/js/content-script.js
+++ markdown-toc-code/js/content-script.js
@@ -1,4 +1,4 @@
-const $ = function (str) {
+const $ = function(str) {
     return document.querySelector(str);
 };
 
@@ -28,36 +28,46 @@ let $container;
 let $repoheadDetailsContainer;
 let $jsRepoNav;
 let containerClient;
-const domInit = function(){
-    console.log(new Date().getTime(),'初始化开始')
-    var git_reg = /[https://github.com].*/;
-    if (git_reg.test(document.URL)) {
+
+const domInit = function() {
+    // 这里只对 github 域名下的 readme 后缀的生效
+    var mdReg = /^.+github.com.+\.md#*.*$/
+    var repositoryReg = /^.+github.com\/[\w|-]+\/[\w|-]+$/
+
+    // alert(repositoryReg.test(document.URL))
+    if (!(mdReg.test(document.URL) || repositoryReg.test(document.URL))) {
+        return
+    }
+    else{
         var _readme = $('article');
-        var data = _readme.querySelectorAll('h1,h2,h3');
+        var data = _readme.querySelectorAll('h1,h2,h3,h4');
         var div = init(data);
         document.body.appendChild(div);
     }
+
+    // console.log(new Date().getTime(), '初始化开始')
+
     // alert($(window).width);
     console.log(document.getElementById('div_right_bar'));
     bindResize(document.getElementById('div_right_bar'));
     const $idHtml = document.querySelector('#js-repo-pjax-container');
-    $container =  $idHtml.children[1];
+    $container = $idHtml.children[1];
     $repoheadDetailsContainer = document.querySelector('.repohead-details-container');
     $jsRepoNav = document.querySelector('.js-repo-nav');
-    containerClient =  $container.getClientRects()[0];
+    containerClient = $container.getClientRects()[0];
 
-    // 因为你这里你根本不知道页面结构到底加载好了没 资源  z加载enme完毕了吗
+    // 因为你这里你根本不知道页面结构到底加载好了没 资源加载enme完毕了吗
     // document.querySelector('.Header').style.paddingLeft = '350px';
     initTocBar(350);
     // document.querySelector('#js-repo-pjax-container').style.paddingLeft = '350px';
-    
-    
+
+
     // document.getElementsByTagName('html')[0].style.marginLeft  = "350px";
 }
-document.addEventListener('DOMContentLoaded',domInit,false);
 
+document.addEventListener('DOMContentLoaded', domInit, false);
 
-function initTocBar(initWidth){
+function initTocBar(initWidth) {
     var els = document.getElementById('toc').style;
 
     //宇宙超级无敌运算中...  
@@ -66,51 +76,48 @@ function initTocBar(initWidth){
 
     var minWidth = 200;
     var moveWidth = parseInt(initWidth);
-    if(moveWidth < 200){
+    if (moveWidth < 200) {
         return;
     }
     var $header = document.querySelector('.Header');
-    
-    
-    if(moveWidth > minWidth && initWidth > containerClient.left){
+
+
+    if (moveWidth > minWidth && initWidth > containerClient.left) {
         document.getElementsByTagName('html')[0].style.marginLeft = w2;
         $header.style.paddingLeft = 0 + 'px';
         // $container.style.marginLeft = '10px';
         $container.style.marginLeft = '10px';
         $repoheadDetailsContainer.style.marginLeft = '10px';
         $jsRepoNav.style.marginLeft = '10px';
-    }else{
+    } else {
         $header.style.paddingLeft = moveWidth + 'px';
         document.getElementsByTagName('html')[0].style.marginLeft = 0;
         $container.style.marginLeft = 'auto';
         $repoheadDetailsContainer.style.marginLeft = 'auto';
         $jsRepoNav.style.marginLeft = 'auto';
     }
-    els.width = initTocBar  + 'px';
+    els.width = initTocBar + 'px';
 }
 
 
-
-//  这里是画出 toc的逻辑
+// 这里是画出 toc的逻辑
 function init(list) {
     var toc = document.createElement("div");
-    toc.setAttribute("class", "toc"); 
-    toc.setAttribute("id", "toc"); 
+    toc.setAttribute("class", "toc");
+    toc.setAttribute("id", "toc");
 
     var div_top = document.createElement("div");
-    addStyle(div_top, {"width": "100%"});
+    addStyle(div_top, { "width": "100%" });
     div_top.setAttribute('id', "top");
 
     var div_toc = document.createElement("div");
     div_toc.setAttribute('id', "div_toc");
-    div_toc.setAttribute("class", "div_toc"); 
+    div_toc.setAttribute("class", "div_toc");
 
-    var div_right_bar = document.createElement("div");  // 这是一个 拖动bar
+    var div_right_bar = document.createElement("div"); // 这是一个 拖动bar
     div_right_bar.setAttribute('id', "div_right_bar");
-    div_right_bar.setAttribute("class", "div_right_bar"); 
+    div_right_bar.setAttribute("class", "div_right_bar");
     div_right_bar.textContent = "";
-
-
 
     var toc_ul = document.createElement("ul");
 
@@ -122,22 +129,24 @@ function init(list) {
 
         var hreftagname = header.firstElementChild.hash;
         var level = parseInt(header.tagName.replace('H', ''), 10);
-        
-        if(!isGetFisrtLevel){
+
+        if (!isGetFisrtLevel) {
             firstLevel = level;
             isGetFisrtLevel = true;
 
             var header_p = document.createElement("li");
-            header_p.textContent = "GitHub Markdown TOC";
-            addStyle(header_p, {"listStyle": "none","font-size":"20px","margin-bottom":"10px"});
+            header_p.textContent = "GitHub Markdown TOC v2021.07";
+            addStyle(header_p, { "listStyle": "none", "font-size": "18px", "margin-bottom": "10px" });
 
             var gotop_li = document.createElement("li");
             var gotop_li = document.createElement("li");
-            addStyle(gotop_li, {"listStyle": "none"});
+            addStyle(gotop_li, { "listStyle": "none" });
+
             var gotop_a = document.createElement("a");
-            addStyle(gotop_a, {"color": "#0366d6","textOverflow ": "ellipsis","font-weight":"bold"});
+            addStyle(gotop_a, { "color": "#0366d6", "textOverflow ": "ellipsis", "font-weight": "bold" });
             gotop_a.innerHTML = "▲ GO TOP";
             gotop_a.setAttribute("href", "#");
+
             div_toc.appendChild(header_p);
 
             div_toc.appendChild(gotop_li);
@@ -145,21 +154,25 @@ function init(list) {
         }
 
         var li = document.createElement("li");
-        addStyle(li, {"listStyle": "none"});
+        addStyle(li, { "listStyle": "none" });
 
         var a = document.createElement("a");
-        addStyle(a, {"color": "#0366d6","textOverflow ": "ellipsis"});
+        addStyle(a, { "color": "#0366d6", "textOverflow ": "ellipsis" });
+        var span = document.createElement("span");
+
 
         // a.innerHTML = level + header.textContent;
-        if( level == firstLevel ){
+        // 如果是第一层
+        if (level == firstLevel) {
             a.innerHTML = header.textContent;
-            addStyle(a, {"font-weight":"bold"});
-        }
-        else{
-            a.innerHTML = new Array(level * 2).join('&nbsp;')  + header.textContent;
+            addStyle(a, { "font-weight": "bold" });
+        } else {
+            a.innerHTML = header.textContent;
+            span.innerHTML = new Array(level * 3).join('&nbsp;')
         }
         a.setAttribute("href", hreftagname);
-        // li.appendChild(a);
+        li.appendChild(span)
+        li.appendChild(a);
         div_toc.appendChild(li);
         div_toc.lastChild.appendChild(a);
     }
@@ -171,32 +184,31 @@ function init(list) {
 }
 
 
-// 这里是绑定resize事件的方法
+// 这里是绑定 resize 事件的方法
 function bindResize(el) {
     //初始化参数
     var els = document.getElementById('toc').style;
     //鼠标的 X 和 Y 轴坐标
     x = 0;
-     //邪恶的食指
-    el.onmousedown = function(e){
+    //邪恶的食指
+    el.onmousedown = function(e) {
         //按下元素后，计算当前鼠标与对象计算后的坐标
         x = e.clientX - el.offsetWidth,
-        y = e.clientY - el.offsetHeight;
+            y = e.clientY - el.offsetHeight;
         console.log(x);
         //在支持 setCapture 做些东东
         el.setCapture ? (
-        //捕捉焦点
+            //捕捉焦点
             el.setCapture(),
-        //设置事件
-            el.onmousemove = function (ev)
-            {
+            //设置事件
+            el.onmousemove = function(ev) {
                 mouseMove(ev || event);
             },
             el.onmouseup = mouseUp
         ) : (
-            function(){
-                document.addEventListener('mousemove',mouseMove,false);
-                document.addEventListener('mouseup',mouseUp,false);
+            function() {
+                document.addEventListener('mousemove', mouseMove, false);
+                document.addEventListener('mouseup', mouseUp, false);
             }()
         );
         //防止默认事件发生
@@ -214,7 +226,7 @@ function bindResize(el) {
         // haha xiecuole 
         // els.width = e.clientX - x + 'px';
         // 好的你来操作
-      
+
         // if(e.clientX && e.clientX > (w-980)/2 ){
         //     document.getElementsByTagName('html')[0].style.marginLeft = w2;
         // }else{
@@ -225,43 +237,40 @@ function bindResize(el) {
 
         // }
         // console.log("e.clientX " + e.clientX);
-    
-        
+
+
         // var readmeWidth = document.getElementById("readme").clientWidth;
         // var maxWidth = document.getElementById("readme").clientWidth * 0.8;
         var minWidth = 200;
         var moveWidth = parseInt(e.clientX);
-        if(moveWidth < 200){
+        if (moveWidth < 200) {
             return;
         }
         var $header = document.querySelector('.Header');
-       
-        
-        if(moveWidth > minWidth && e.clientX > containerClient.left){
+
+
+        if (moveWidth > minWidth && e.clientX > containerClient.left) {
             document.getElementsByTagName('html')[0].style.marginLeft = w2;
             $header.style.paddingLeft = 0 + 'px';
             // $container.style.marginLeft = '10px';
             $container.style.marginLeft = '10px';
             $repoheadDetailsContainer.style.marginLeft = '10px';
             $jsRepoNav.style.marginLeft = '10px';
-        }else{
+        } else {
             $header.style.paddingLeft = moveWidth + 'px';
             document.getElementsByTagName('html')[0].style.marginLeft = 0;
             $container.style.marginLeft = 'auto';
             $repoheadDetailsContainer.style.marginLeft = 'auto';
             $jsRepoNav.style.marginLeft = 'auto';
         }
-        els.width = e.clientX  + 'px';
+        els.width = e.clientX + 'px';
 
         return;
-        if( moveWidth > minWidth &&  e.clientX > (w-980)/2 ){
+        if (moveWidth > minWidth && e.clientX > (w - 980) / 2) {
             document.getElementsByTagName('html')[0].style.marginLeft = w2;
             $header.style.paddingLeft = 0 + 'px';
-           
         }
-
-
-        els.width = e.clientX  + 'px';
+        els.width = e.clientX + 'px';
     }
     //停止事件
     function mouseUp() {
@@ -272,10 +281,10 @@ function bindResize(el) {
             //移除事件
             el.onmousemove = el.onmouseup = null
         ) : (
-                function(){
-                    document.removeEventListener("mousemove", mouseMove);
-                    document.removeEventListener("mouseup", mouseUp);
-                }()               
-            );
+            function() {
+                document.removeEventListener("mousemove", mouseMove);
+                document.removeEventListener("mouseup", mouseUp);
+            }()
+        );
     }
 }
